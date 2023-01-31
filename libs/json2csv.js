052fac70d48fdd9268ba9ae38279cbed99692367
Yusuke <y-maeda@recus-groove.com>
2023-01-31 12:16:32 +0900

    フィールドの自動取得

--- libs/json2csv.js
+++ libs/json2csv.js
@@ -1,16 +1,33 @@
-const { createReadStream, createWriteStream } = require('fs');
+const { createReadStream, createWriteStream, readFileSync } = require('fs');
 const { Transform } = require('json2csv');
 
-// 各項目は手動で設定する必要があります。
-const fields = ['連番', '氏名', '氏名（カタカナ）', '性別', '電話番号', '生年月日'];
-// Windowsで文字化けしないようにオプションを追加します。
-const opts = { fields, withBOM: true };
-const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
+// 各項目は手動で設定する必要があります。-> 自動化（※深度1だけなので注意）
+// let fields = ['連番', '氏名', '氏名（カタカナ）', '性別', '電話番号', '生年月日'];
+let fields = [];
 
 // 入力パスが未定義なので定義します。
 const inputPath = 'json/data.json';
+// jsonファイルのテキストを取得
+const jsonText = readFileSync(inputPath, 'utf-8');
+// keyの取得
+const jsonParsed = JSON.parse(jsonText);
+// const keyList = Object.keys(jsonParsed); // -> key=0 | value=[object Object] こうなる。keyは数字がインクリメントする
+
+// for (let key in keyList) {
+//   // 取得したkeyとvalueの確認
+//   console.log(`key=${keyList[key]} | value=${jsonParsed[keyList[key]]} `);
+// }
+
+// 0番目（最初の1個)のkeyを取得する
+fields = Object.keys(jsonParsed[0]);
+// console.log(`chideKeyList=${Object.keys(jsonParsed[0])} `);
+
 // 出力パスが未定義なので定義します。
 const outputPath = 'csv/data.csv';
+
+// Windowsで文字化けしないようにオプションを追加します。
+const opts = { fields, withBOM: true };
+const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
 
 const input = createReadStream(inputPath, { encoding: 'utf8' });
 const output = createWriteStream(outputPath, { encoding: 'utf8' });
